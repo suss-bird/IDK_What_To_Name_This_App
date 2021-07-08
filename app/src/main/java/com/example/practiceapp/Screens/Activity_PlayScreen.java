d0e6b17bd0559c5c9a2056b901611a6c3c436e3f
s3d-bird <arslank3377@gmail.com>
2021-07-08 11:57:41 +0500

    Seekbar works; play and pause button works

--- app/src/main/java/com/example/practiceapp/Screens/Activity_PlayScreen.java
+++ app/src/main/java/com/example/practiceapp/Screens/Activity_PlayScreen.java
@@ -4,17 +4,22 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.MediaController;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.practiceapp.MainActivity;
 import com.example.practiceapp.R;
 
 import java.io.IOException;
 
 public class Activity_PlayScreen extends AppCompatActivity {
+    boolean playPause;
+
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +42,59 @@ public class Activity_PlayScreen extends AppCompatActivity {
             e.printStackTrace();
         }
         mediaPlayer.start();
+        playPause = true;
+
+        findViewById(R.id.btn_PlayPause).setOnClickListener(v -> {
+            if(playPause) {
+                mediaPlayer.pause();
+                ((ImageView) v).setImageResource(R.drawable.ic_playbtn);
+                playPause = false;
+            }
+            else {
+                mediaPlayer.start();
+                ((ImageView) v).setImageResource(R.drawable.ic_pausebtn);
+                playPause = true;
+                // works like a charm xd
+            }
+        });
+
+        findViewById(R.id.btn_FF).setOnClickListener(v -> {
+            mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 10);
+        });
+        ((SeekBar)findViewById(R.id.seekbar_main)).setMax(mediaPlayer.getDuration());
+
+        Handler mHandler = new Handler();
+        Activity_PlayScreen.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if(mediaPlayer != null){
+                    int mCurrentPosition = mediaPlayer.getCurrentPosition();
+                    ((SeekBar) findViewById(R.id.seekbar_main)).setProgress(mCurrentPosition);
+                }
+                mHandler.postDelayed(this, 1000);
+            }
+        });
+
+        ((SeekBar)findViewById(R.id.seekbar_main)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
 }
